f04675e916a7e855122965369b4582c1f7aabf8d
nilsdiedrich <nils.diedrich@aol.de>
2019-12-02 22:12:31 +0100

    added Stanford features

--- feature_engineering/annotate.py
+++ feature_engineering/annotate.py
@@ -1,8 +1,10 @@
 from stanford_sentimentals import StanfordAnnotator
+from progress_bar import ProgressBar
 from lexical_features import LexicalAnnotator
 import glob
 import os.path
 import pandas as pd
+import time
 
 train_folder = "../data/datasets/train-articles"
 
@@ -24,6 +26,10 @@ data['em_size'] = ''
 data['ner_person'] = ''
 data['ner_organization'] = ''
 data['ner_country'] = ''
+
+start_time = time.time()
+sa = StanfordAnnotator('http://localhost', 9000)
+total = data.shape[0]
 for index, row in data.iterrows():
     article = articles.get(str(row[0]))
     propaganda = article[row[1]:row[2]]
@@ -33,10 +39,14 @@ for index, row in data.iterrows():
     data.at[index, 'sequence_size'] = "%.2f" % lexical[0]
     data.at[index, 'em_size'] =  lexical[1]
     data.at[index, 'qm_size'] = lexical[2]
-    continue
-    #NER ANNOTATION
-    sa = StanfordAnnotator('http://localhost', 9000)
-    ner = sa.annotate_ner(propaganda)
-    data.at[index, 'ner_person'] = ner[0]
 
+    #NER ANNOTATION
+    ner = sa.annotate_ner(propaganda)
+    data.at[index, 'ner_person'] = ', '.join(ner[0])
+    data.at[index, 'ner_organization'] = ', '.join(ner[1])
+    data.at[index, 'ner_country'] = ', '.join(ner[2])
+
+    ProgressBar.printProgressBar(index, total)
+
+print("\n DONE! : " +str(time.time()-start_time))
 data.to_csv('annotated.csv',index=False)
