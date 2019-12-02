e6ad6e015e65dc4e01e74f1c076ee00138244311
Iliass <aymaz.iliass@gmail.com>
2019-12-03 00:44:00 +0100

    added topic per document feature

--- LDA/LDA.py
+++ LDA/LDA.py
@@ -13,6 +13,7 @@ from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 import gensim
 import os
+import re
 
 
 def preprocess(articles):
@@ -56,7 +57,8 @@ def preprocess(articles):
     # print('Into a bag of words', bg_words)
     return bg_words, dictionary
 
-def lda(bg_words, dictionary, num_topics, num_words):
+
+def lda(bg_words, dictionary, num_topics, num_words, rootdir):
     """
     Generates the LDA model.
     Takes the dictionary and the bag of words
@@ -72,6 +74,21 @@ def lda(bg_words, dictionary, num_topics, num_words):
         n = len(files)
     print('Executed in ' + str(round(end-start, 2)) + ' seconds on ', len([iq for iq in os.scandir(rootdir)]), 'files')
     # print(lda_model.print_topics(num_topics=num_topics, num_words=num_words))
+    return lda_model, bg_words
+
+
+def identify_docs_by_topic(lda_model, bg_words, article_names):
+    # each document is defined by its bag of words
+    # we use this to get the classification result on some document
+    if input('Show documents by topic? [y/n]') == 'y':
+        for i in range(len(bg_words)):
+            for j in range(len(lda_model[bg_words[i]])):
+                # put that as dataframe afterwards
+                print('Document article%s.txt has a probability of %f%% to be in the topic %d' %
+                      (article_names[i], lda_model[bg_words[i]][j][1]*100,
+                       lda_model[bg_words[i]][j][0]))
+    else:
+        pass
 
 
 def main():
@@ -80,13 +97,20 @@ def main():
 
     # Get articles from the dataset
     articles = []
+    # list to store article names
+    article_names = []
     for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
+        for file in sorted(files):
+            # match the number in the article name and store it in the list
+            article_names.append(re.findall(r'\d+', file)[0])
             with open(os.path.join(subdir, file), 'r') as article:
                 articles.append(article.read())
 
     bag_words, dict_ = preprocess(articles)
-    lda(bag_words, dict_, 14, 7)
+    lda_model, bag_words = lda(bag_words, dict_, 10, 5, rootdir)
+
+    # Get documents by topic
+    identify_docs_by_topic(lda_model, bag_words, article_names)
 
 
 if __name__ == '__main__':
