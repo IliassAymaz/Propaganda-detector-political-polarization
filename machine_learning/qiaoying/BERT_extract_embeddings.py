19df57579ffd87004c8ac618d6eccaf67de9628a
Qiaoying Lin <563660207@qq.com>
2020-01-13 19:20:06 +0100

    method to get bert embedding

--- machine_learning/qiaoying/BERT_extract_embeddings.py
+++ machine_learning/qiaoying/BERT_extract_embeddings.py
@@ -6,53 +6,40 @@ from transformers import BertTokenizer, BertForTokenClassification
 import pandas as pd
 import numpy as np
 
-print(torch.__version__)
-train_table = pd.read_csv("train_table.csv")
-train_table['hidden_states'] = ''
-train_table['loss'] = ''
-
-#define the tokenizer & the model
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForTokenClassification.from_pretrained('bert-base-uncased', output_hidden_states=True)
-model.eval()
-print(model.config)
-
-sentence_embeddings = {}
-non_propaganda_table = {}
-for index, row in train_table.iterrows():
-    print(index)
-    #sentence_tokens
-    train_sentence = row[1]
-    train_tokens = tokenizer.tokenize(train_sentence)
-    train_tokens_ids = tokenizer.convert_tokens_to_ids(train_tokens)
-    train_tokens_ids_tensor = torch.tensor([train_tokens_ids])
+def convert_into_bert_tokens(model, tokenizer, sentence, span):
+    #convert sentence into tokens
+    sentence_tokens = tokenizer.tokenize(sentence)
+    sentence_tokens_ids = tokenizer.convert_tokens_to_ids(sentence_tokens)
+    sentence_tokens_ids_tensor = torch.tensor([sentence_tokens_ids])
     
-    #label_tokens
-    spans = row[2]
+    #convert label into tokens
     begin_offset = -1
     end_offset = -1
-    if pd.isnull(spans):
-        train_labels = [0 for i in range(len(train_tokens_ids))]
-        train_labels_tensor = torch.tensor([train_labels])
+    if pd.isnull(span):
+        #non-propaganda
+        labels = [0 for i in range(len(sentence_tokens_ids))]
+        labels_tensor = torch.tensor([labels])
     else:
-        spans_tokens = tokenizer.tokenize(spans)
-#        print(repr(train_sentence))
-#        print(repr(spans))
-        spans_tokens_ids = tokenizer.convert_tokens_to_ids(spans_tokens)
-        begin_offset = train_tokens_ids.index(spans_tokens_ids[0])
-        end_offset = train_tokens_ids.index(spans_tokens_ids[-1]) + 1
+        #contains propaganda
+        span_tokens = tokenizer.tokenize(span)
+        span_tokens_ids = tokenizer.convert_tokens_to_ids(span_tokens)
+        begin_offset = sentence_tokens_ids.index(span_tokens_ids[0])
+        end_offset = sentence_tokens_ids.index(span_tokens_ids[-1]) + 1
         
-        train_labels = [1 if i < end_offset and i >= begin_offset else 0 for i in range(len(train_tokens_ids))]
-        train_labels_tensor = torch.tensor([train_labels])
+        labels = [1 if i < end_offset and i >= begin_offset else 0 for i in range(len(sentence_tokens_ids))]
+        labels_tensor = torch.tensor([labels])
+        
+    return sentence_tokens_ids_tensor, labels_tensor, begin_offset, end_offset
     
-    #use the pre-trained model to get token embeddings
+def get_bert_embedding(model, sentence_tokens, labels_tensor, begin_offset, end_offset):
+    #use the pre-trained model to get sentence embeddings
     with torch.no_grad():
-        outputs = model(train_tokens_ids_tensor, labels=train_labels_tensor)
+        outputs = model(sentence_tokens, labels=labels_tensor)
         loss, scores, hidden_states = outputs[:3]
         
-        #hidden_states[layer][0][token][units]
+        #hidden_states[layer][0][token][units], 768 units for each token
         token_embeddings = []
-        for token in range(len(train_tokens)):
+        for token in range(len(sentence_tokens)):
             hidden_layers = []
             for layer in range(len(hidden_states)):
                 hidden_layers.append(hidden_states[layer][0][token])
@@ -65,15 +52,43 @@ for index, row in train_table.iterrows():
         
         #propagandistic span embeddings
         if begin_offset != -1:
-            sentence_embeddings[train_sentence] = (spans, row[3], row[4], token_embeddings_all_layers[begin_offset:end_offset])
+            span_embeddings = token_embeddings_all_layers[begin_offset:end_offset]
         #non-propagandistic span embeddings
         else:
-            sentence_embeddings[train_sentence] = token_embeddings_all_layers[:]
-            non_propaganda_table[train_sentence] = -1
+            span_embeddings = token_embeddings_all_layers[:]
+    return span_embeddings
 
+def main():
+    data_table = pd.read_csv("train_table.csv")
+    
+    #define bert tokenizer && bert model
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForTokenClassification.from_pretrained('bert-base-uncased', output_hidden_states=True)
+    model.eval()
+    print(model.config)
+    
+    data_embeddings = {}
+    for index, row in data_table.iterrows():
+        print(index)
+        #get sentence_tokens && label_tokens
+        sentence = row[1]
+        span = row[2]
+        sentence_tokens, label_tokens, begin_offset, end_offset = convert_into_bert_tokens(model, tokenizer, sentence, span)
+        
+        #get bert embeddings
+        span_embeddings = get_bert_embedding(model, sentence_tokens, label_tokens, begin_offset, end_offset)
 
-print("Writing to csv file...")
-pd.DataFrame.from_dict(data=sentence_embeddings, orient='index').to_csv('train_table_embeddings.csv', header=False, encoding = "utf-8")
-#torch.save(sentence_embeddings, "span_embeddings.pt")
-pd.DataFrame.from_dict(data=non_propaganda_table, orient='index').to_csv('non_propaganda_table.csv', header=False, encoding = "utf-8")
-print("Done.")
+        #propagandistic span embeddings
+        if begin_offset != -1:
+            data_embeddings[sentence] = (span, row[3], row[4], span_embeddings)
+        #non-propagandistic span embeddings
+        else:
+            data_embeddings[sentence] = span_embeddings
+        
+    print("Writing to output file...")
+    torch.save(data_embeddings, "data_embeddings.pt")
+    print("Done.")
+
+if __name__ == "__main__":
+    print(torch.__version__)
+    main()
